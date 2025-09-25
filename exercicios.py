7bece854ebd1ff87bdc328ac958d396ccf4fcc70
Bruno-Botelho <bruno.botelho@live.com>
2025-09-24 23:53:36 -0300

    incluido o exercicio 4

--- exercicios.py
+++ exercicios.py
@@ -4,13 +4,13 @@
 # Escreva um programa que verifique esses campos e imprima "Dados válidos" se ambos 
 # forem positivos ou "Dados inválidos" caso contrário.
 
-quantidades = [1,2,3,4,-2]
+# quantidades = [1,2,3,4,-2]
 
-for quantidade in quantidades:
-    if quantidade < 0:
-        print(f'Dado invalido: {quantidade}')
-    else:
-        print(f'Dados correto: {quantidade}')
+# for quantidade in quantidades:
+#     if quantidade < 0:
+#         print(f'Dado invalido: {quantidade}')
+#     else:
+#         print(f'Dados correto: {quantidade}')
 
 
 
@@ -19,15 +19,15 @@ for quantidade in quantidades:
 # Os dados incluem medições de temperatura. Você precisa classificar cada leitura 
 # como 'Baixa', 'Normal' ou 'Alta'. Considerando que:
 
-temp = [12,15,20,99,49,45]
+# temp = [12,15,20,99,49,45]
 
-for temperatura in temp:
-    if temperatura <10:
-        print('Baixa')
-    elif temperatura <= 30:
-        print('Normal')
-    else:
-        print('ALTAAA')
+# for temperatura in temp:
+#     if temperatura <10:
+#         print('Baixa')
+#     elif temperatura <= 30:
+#         print('Normal')
+#     else:
+#         print('ALTAAA')
 
 ### Exercício 3: Filtragem de Logs por Severidade
 # Você está analisando logs de uma aplicação e precisa filtrar mensagens 
@@ -35,12 +35,56 @@ for temperatura in temp:
 # como `log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}`, 
 # escreva um programa que imprima a mensagem se a severidade for 'ERROR'.
 
+# logs = [
+#     {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'},
+#     {'timestamp': '2021-06-23 10:05:00', 'level': 'INFO', 'message': 'Conexão estabelecida'},
+#     {'timestamp': '2021-06-23 10:10:00', 'level': 'WARNING', 'message': 'Latência alta detectada'},
+#     {'timestamp': '2021-06-23 10:15:00', 'level': 'ERROR', 'message': 'Servidor não respondeu'},
+#     {'timestamp': '2021-06-23 10:20:00', 'level': 'INFO', 'message': 'Processamento concluído'},
+#     {'timestamp': '2021-06-23 10:25:00', 'level': 'ERROR', 'message': 'Falha ao salvar no banco de dados'}
+# ]
+# var = 'teeste'
+# for log in logs:
+#     if log['level'] == 'ERROR':
+#         novo_log = {
+#         'timestamp': '2021-06-23 10:30:00',
+#         'level': var,
+#         'message': 'Novo registro adicionado',
+#         'usuario': 'sistema'
+#     }
+#         logs.append(novo_log)
+        
+#         print(logs)
+
 ### Exercício 4: Validação de Dados de Entrada
 # Antes de processar os dados de usuários em um sistema de recomendação, 
 # você precisa garantir que cada usuário tenha idade entre 18 e 65 anos e tenha 
 # fornecido um email válido. Escreva um programa que valide essas condições 
 # e imprima "Dados de usuário válidos" ou o erro específico encontrado.
 
+import re
+
+while True:
+    try:
+        idade = int(input('Digite sua idade: '))
+        break
+    except:
+        print('Digite um numero certo MMULA!!')
+
+while True:
+    
+    email = input('Digite seu email: ')
+
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            print('Digite um email válido!')
+    else:
+        break
+    
+# Validação final
+if 18 <= idade <= 65:
+    print("Dados de usuário válidos")
+else:
+    print("Erro: idade fora do intervalo permitido (18 a 65 anos)")
 ### Exercício 5: Detecção de Anomalias em Dados de Transações
 # Você está trabalhando em um sistema de detecção de fraude e precisa identificar 
 # transações suspeitas. Uma transação é considerada suspeita se o valor for superior 
